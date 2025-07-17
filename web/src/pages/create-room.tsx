85c1224ff0c1ead18ac56970c545ddfedc378905
limaarthur <artglima.dev@gmail.com>
2025-07-17 15:50:01 -0300

    feat: api - fetch rooms from api

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,9 +1,26 @@
+import { useQuery } from '@tanstack/react-query'
 import { Link } from 'react-router-dom'
 
+type GetRoomsAPIResponse = Array< { // Determina que a nossa API será um array de objetos com id e name
+  id: string
+  name: string
+}>
+
 export function CreateRoom() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['get-rooms'], // Identificador único para a chamada HTTP
+    queryFn: async () => { // Função que será executada para trazer os dados da API
+      const response = await fetch('http://localhost:3333/rooms') // Faz a chamada a API
+      const result: GetRoomsAPIResponse = await response.json() // Converte para JSON
+
+      return result
+    }, 
+  })
   return (
     <div>
       <div>Create Room</div>
+
+      {isLoading && <p>Carregando...</p>}
 
       <Link className="underline" to="/room">Acessar sala</Link>
     </div>
